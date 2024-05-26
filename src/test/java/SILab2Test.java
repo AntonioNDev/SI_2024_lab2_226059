38275be70ff95ec980ad4fe7325c9020008be713
AntonioNDev <antonionikoloski2003@gmail.com>
2024-05-26 23:56:06 +0200

    changed readme

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -8,8 +8,8 @@ class SILab2Test {
 
     @Test
     void checkCart() {
-        assertTrue(SILab2.checkCart(new Item("1234567890", "Item1", 10, 0.1), 25));
-        assertTrue(SILab2.checkCart(new Item("", "", 15, 0.2), 15));
+        assertTrue(SILab2.checkCart((List<Item>) new Item("1234567890", "Item1", 10, 0), 25));
+        assertTrue(SILab2.checkCart((List<Item>) new Item("", "", 15, 0), 15));
         assertTrue(SILab2.checkCart((List<Item>) new Item(null, "Item3", 50, 0), 50));
     }
 }
